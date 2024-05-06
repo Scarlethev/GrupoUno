285d51e207023706046826abea61d6156fa6f7cf
Scarlethev <scarleth_07@hotmail.com>
2024-05-06 10:54:00 -0300

    RutasOK

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Inicio from "./components/Inicio/Inicio";
-import { Servicios } from "./components/Servicios/Servicios";
+import {Servicios} from "./components/Servicios/Servicios";
 import Footer from "./components/Footer/Footer";
 import Sugerencias from "./components/Sugerencias";
 import { BrowserRouter, Routes, Router, Route} from 'react-router-dom'
@@ -11,17 +11,15 @@ function App() {
 
   return (
     <>
-<Header/>
-
 <BrowserRouter>
+<Header/>
 <Routes>
 <Route path= "/" element={<Inicio/>}/>
-<Route path= "/servicio" element={<Servicios/>}/>
+<Route path= "/servicios" element={<Servicios/>}/>
 <Route path= "/sugerencias" element={<Sugerencias/>}/>
 </Routes>
-</BrowserRouter>
-
 <Footer />
+</BrowserRouter>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import './header.css'
 import {Link} from 'react-router-dom'
 
-
 const Header=({})=>{
 
-    const lista =[{name: 'Inicio', url:'/'}, {name: 'Servicios', url: '/servicios'},{name: 'Sugerencias', url: '/sugerencias'}]
+    const lista =[{name:'Inicio', url:'/'}, {name:'Servicios', url:'/servicios'},{name:'Sugerencias', url:'/sugerencias'}]
 
+    let ul=lista.map((item,index)=>
+        (
+    <li key={index}><Link to={item.url} className='ancla-menu'>{item.name}</Link></li>
+    ))
 
     return(
         <>
-        <ul>{lista.map((item,index)=>
-                (
-            <li key={index}><Link to={item.url} className='ancla-menu'>{item.name}</Link></li>
-            ))}</ul>
+        <ul>{ul}</ul>
         </>
 
         )
