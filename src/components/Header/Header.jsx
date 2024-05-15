b0a1e8201437f287d4353f036d489710ecf85996
Nelson Bermejo <bermejonelson.r@gmail.com>
2024-05-15 15:21:56 -0300

    menu-hamburguesa init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,38 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const lista = [
     { name: "Inicio", url: "/" },
     { name: "Servicios", url: "/servicios" },
     { name: "Sugerencias", url: "/sugerencias" },
   ];
 
-    let ul = lista.map((item,index)=>
-        (
-    <li key={index}><Link to={item.url} className='ancla-menu'>{item.name}</Link></li>
-    ))
+  let ul = lista.map((item, index) => (
+    <li key={index}>
+      <Link to={item.url} className="ancla-menu" onClick={() => setIsOpen(false)}>
+        {item.name}
+      </Link>
+    </li>
+  ));
 
-    return(
-        <>
-        <ul>{ul}</ul>
-        </>
-
-        )
-    }
+  return (
+    <>
+      <header className="header">
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
+        <ul className={`nav-links ${isOpen ? 'active' : ''}`}>{ul}</ul>
+      </header>
+    </>
+  );
+};
 
 export default Header;
